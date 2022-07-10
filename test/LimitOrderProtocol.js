d873856df0205732c3b70dbb64029109ac65f764
ABresting <abhimanyur2010@gmail.com>
2022-07-10 12:56:00 +0200

    test case updated

--- test/LimitOrderProtocol.js
+++ test/LimitOrderProtocol.js
@@ -43,7 +43,22 @@ contract('LOP', async function (accounts) {
 
     });
     
-    it('full test suite', async function(){
+    it('NFT test - no swap', async function(){
+        this.dai = await TokenMock.new('DAI', 'DAI');
+        this.weth = await WrappedTokenMock.new('WETH', 'WETH');
+
+        this.swap = await LimitOrderProtocol.new(this.weth.address);
+
+        await this.dai.mint(addr0, '1000000');
+        await this.dai.mint(addr1, '1000000');
+        await this.weth.deposit({ from: addr0, value: '1000000' });
+        await this.weth.deposit({ from: addr1, value: '1000000' });
+
+        await this.dai.approve(this.swap.address, '1000000');
+        await this.weth.approve(this.swap.address, '1000000');
+        await this.dai.approve(this.swap.address, '1000000', { from: addr1 });
+        await this.weth.approve(this.swap.address, '1000000', { from: addr1 });
+
         this.collection = await NFTCollection.new();
         this.firstNFT = await this.collection.mintNFT("Test NFT", "test.uri.domain.io", { from: addr1 });
 
@@ -92,6 +107,76 @@ contract('LOP', async function (accounts) {
             );
         });
 
+
+    it('NFT test - with swap', async function(){
+        this.dai = await TokenMock.new('DAI', 'DAI');
+        this.weth = await WrappedTokenMock.new('WETH', 'WETH');
+
+        this.swap = await LimitOrderProtocol.new(this.weth.address);
+
+        await this.dai.mint(addr0, '1000000');
+        await this.dai.mint(addr1, '1000000');
+        await this.weth.deposit({ from: addr1, value: '1000000' });
+
+        await this.dai.approve(this.swap.address, '1000000');
+        await this.weth.approve(this.swap.address, '1000000');
+        await this.dai.approve(this.swap.address, '1000000', { from: addr1 });
+        await this.weth.approve(this.swap.address, '1000000', { from: addr1 });
+
+        this.collection = await NFTCollection.new();
+        this.firstNFT = await this.collection.mintNFT("Test NFT", "test.uri.domain.io", { from: addr1 });
+
+        console.log('create NFT limit sell order  ');
+        console.log(addr0,'weth balance (pre-trade):',  Number(await this.weth.balanceOf(addr0)))
+        console.log(addr1,'weth balance (pre-trade):', Number(await this.weth.balanceOf(addr1)))
+        console.log(addr0,'NFT balance (pre-trade):', Number(await this.collection.balanceOf(addr0)))
+        console.log(addr1,'NFT balance (pre-trade):', Number(await this.collection.balanceOf(addr1)))
+        
+        /* approve NFT */
+        await this.collection.approve(this.swap.address, 0, { from: addr1 })
+
+        
+            const sentOrder1 = buildOrder(
+                {
+                    makerAsset: this.collection.address,
+                    takerAsset: this.weth.address,
+                    NFTID: 0,
+                    takingAmount: 2,
+                    from: addr1,
+                },
+            );
+            const signature1 = signOrder(sentOrder1, Number(this.chainId), this.swap.address, addr1Wallet.getPrivateKey());
+
+            if (Number(await this.weth.balanceOf(addr0)== 0 )){
+                console.log("NFT buying token is not available to the user!")
+                console.log("Get the WETH i.e. NFT asking currency, swap DAI for ETH, using 1inch swap")
+                // mimicking the swap behavior
+                await this.weth.deposit({ from: addr0, value: '1000000' });
+            }
+
+            await this.swap.fillOrderNFTnoSwap(sentOrder1, signature1, '0x',{ from: addr0 });
+            
+            console.log(addr0,'weth balance (post-trade1):',  Number(await this.weth.balanceOf(addr0)))
+            console.log(addr1,'weth balance (post-trade1):', Number(await this.weth.balanceOf(addr1)))
+
+            console.log(addr0,'NFT balance (post-trade1):', Number(await this.collection.balanceOf(addr0)))
+            console.log(addr1,'NFT balance (post-trade1):', Number(await this.collection.balanceOf(addr1)))
+
+
+            console.log('create NFT limit buy order. Buy it back for a loss ');
+
+        /* approve NFT, again*/
+        await this.collection.approve(this.swap.address, 0, { from: addr0 })
+            const sentOrder2 = buildOrder(
+                {
+                    makerAsset: this.weth.address,
+                    takerAsset: this.collection.address,
+                    NFTID: 0,
+                    takingAmount: 3,
+                    from: addr1,
+                },
+            );
+        });
 
     
     
